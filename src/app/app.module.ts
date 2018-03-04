439cf94ecd64b3221d4b0836e16c147ae739672e
abou hassoun <hussien@macbook.com>
2018-03-04 12:28:19 +0300

    add new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HusseinComponent } from './components/hussein/hussein.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HusseinComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/hussein/hussein.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hussein',
+  templateUrl: './hussein.component.html',
+  styleUrls: ['./hussein.component.css']
+})
+export class HusseinComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
